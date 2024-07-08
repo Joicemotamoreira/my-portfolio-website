52f784dabd9c5fcc43aa27c2e92e33fa7338dbc9
Joicemotamoreira <joicemotamoreira@live.com>
2024-07-08 16:02:05 +0100

    Fixed the copy buttons functions

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function myFunction() {
+function myFunctionMail() {
+  let copyText = document.getElementById("myMail");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+}
+
+function myFunctionNumber() {
   let copyText = document.getElementById("myNumber");
 
   copyText.select();
